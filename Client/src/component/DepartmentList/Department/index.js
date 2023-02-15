b78339fbca9ca2bd16f2c0ea434714d5f502cb51
fapvelder <lyhungphat25@gmail.com>
2023-02-16 06:28:19 +0700

    new-ideabox

--- Client/src/component/DepartmentList/Department/index.js
+++ Client/src/component/DepartmentList/Department/index.js
@@ -1,18 +1,20 @@
 import React from "react";
-import {  Typography } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 
-export default function Department({department}) {
-    return (
-        <li>
-        <div className="column">
-          <div className="icon">
-            <i className="fas fa-globe-asia"></i>
-          </div>
-          <div className="details">
-          <Typography variant="h5" color="textPrimary">{department.name}</Typography>
-          </div>
+export default function Department({ department }) {
+  return (
+    <li style={{ marginLeft: "100px" }}>
+      <div className="column">
+        <div className="icon">
+          <i className="fas fa-globe-asia"></i>
         </div>
-        <div className="radio"></div>
-      </li>
-    );
+        <div className="details">
+          {/* <Typography variant="h5" color="textPrimary">
+            {department.name}
+          </Typography> */}
+        </div>
+      </div>
+      <div className="radio"></div>
+    </li>
+  );
 }
